045d9a9691c5e5aa4fc2837d7c79d391467f17be
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-13 11:43:05 -0700

    using middleware

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express"),
 app = express(),
 port = process.argv[2] || 3000;
 
+//middleware - it will log date everytime a request is recieved
+app.use(function(req, res, next){
+    console.log("Recieved request at "+ Date.now());
+    next();
+})
+
 app.get('/hello', function (req, res) {
     let message = "Hello World";
     res.send(message);
@@ -11,6 +17,11 @@ app.get('/', function (req, res) {
     let message = "Home";
     res.send(message);
 })
+
+
+
+//you can serch files from a directory named public
+app.use(express.static('public')); // we have test.html in public directory. It will rendered with url localhost:3000/test.html we dont need to provide the directory name in the path
 
 app.get('/randomnumber', function (req, res) {
     //use url /randomnumber?range=40
@@ -22,6 +33,11 @@ app.get('/randomnumber', function (req, res) {
 app.get('/goodbye', function (req, res) {
     let message = "Goodbye World";
     res.send(message);
+})
+
+app.get('/json', function (req, res) {
+    let json = {name:"hello"};
+    res.json(json);
 })
 
 app.listen(port, function(){
@@ -37,5 +53,5 @@ app.get('/sum', function(req, res) {
     for (i=0; i<= n; i++) {
         sum += i;
     }
-    res.send("The sum of 1 to "+ n + " is: "+ sum);
+    res.send("The sum of 1 to "+ n + " is: <strong>"+ sum +"</strong>");
 })
