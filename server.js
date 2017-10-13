6ae949dc2faa21e35aa2cd944d58b0cdc6c708f0
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-13 11:17:49 -0700

    listening to multiple server ports

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const express = require("express");
-const app = express();
+const express = require("express"),
+app = express(),
+port = process.argv[2] || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello');
+app.get('/hello', function (req, res) {
+    let message = "Hello World";
+    res.send(message);
 })
 
-app.listen(3000, function(){
-    console.log("Listening");
+app.get('/', function (req, res) {
+    let message = "Home";
+    res.send(message);
+})
+
+app.get('/randomnumber', function (req, res) {
+    res.send((Math.floor(Math.random()*10)).toString());
+})
+
+app.get('/goodbye', function (req, res) {
+    let message = "Goodbye World";
+    res.send(message);
+})
+
+app.listen(port, function(){
+    console.log("Listening to: "+ port);
 });
